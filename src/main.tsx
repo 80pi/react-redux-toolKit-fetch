6ab66733abd2149d905e32d805abf89a31e6f9e7
80pi <gopivenkataajay5771@gmail.com>
2024-04-09 08:51:38 +0530

    adding the store with api intergration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { store } from "./store.ts";
+import { Provider } from "react-redux";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+import { usersApi } from "./slice/userSlice.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ApiProvider api={usersApi}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ApiProvider>
+  </React.StrictMode>
+);
