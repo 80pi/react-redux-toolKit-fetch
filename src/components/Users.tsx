3d2258e567b72be86f7d7dca068a366589835230
80pi <gopivenkataajay5771@gmail.com>
2024-04-09 09:11:17 +0530

    main file

---
+++ src/components/Users.tsx
@@ -0,0 +1,99 @@
+import {
+  useAddPostMutation,
+  useUpdatePostMutation,
+  useAddProductMutation,
+  useUpdateProductMutation,
+} from "../slice/userSlice";
+import { MultipleUsers, SingleUserByInput } from "./AllUsers";
+import { PostAComment, UpdateComment } from "./CommentsSection";
+import { AddProducts, UpdateProducts } from "./ProductsSection";
+
+const Users = () => {
+  const [addPost, { data, isLoading }] = useAddPostMutation();
+  const [updatePost, { data: updateData, isLoading: updatedLoading }] =
+    useUpdatePostMutation();
+
+  const [addProduct, { data: addedProductData, isSuccess: productAddStatus }] =
+    useAddProductMutation();
+  const [
+    updateProduct,
+    { data: updatedProductData, isSuccess: productUpdatedStatus },
+  ] = useUpdateProductMutation();
+
+  console.log("add post", data, isLoading);
+  console.log("update post", updateData, updatedLoading);
+  console.log("data", addedProductData, productAddStatus);
+  console.log("data", updatedProductData, productUpdatedStatus);
+
+  const handlePost = async () => {
+    try {
+      const postData: any = {
+        userId: 10,
+        id: 101,
+        title: "post test",
+        body: "post body",
+      };
+      await addPost(postData);
+    } catch (error: any) {
+      console.log("error while post", error);
+    }
+  };
+  const handleUpdatePost = async () => {
+    try {
+      const updatedPostData: any = {
+        userId: 10,
+        id: 1,
+        body: "post body",
+        title: "post test update",
+      };
+      await updatePost({ id: 1, updatedData: updatedPostData });
+    } catch (error: any) {
+      console.log("error while post", error);
+    }
+  };
+  const handleAddProduct = async () => {
+    try {
+      const newProductData: any = {
+        title: "Gopi Iphone",
+      };
+      await addProduct(newProductData);
+    } catch (error: any) {
+      console.log("error while post", error);
+    }
+  };
+  const handleUpdateProduct = async () => {
+    try {
+      const updatedProductData: any = {
+        title: "Gopi Iphone 15 pro max",
+      };
+      await updateProduct({ id: 1, updatedData: updatedProductData }); //in which if we add getting 201 btu we can't update it
+    } catch (error: any) {
+      console.log("error while post", error);
+    }
+  };
+  return (
+    <>
+      <SingleUserByInput />
+      <MultipleUsers />
+
+      {/* post a comment post api call mutation */}
+      <PostAComment handlePost={handlePost} />
+      <br />
+      <UpdateComment handleUpdatePost={handleUpdatePost} />
+
+      <br />
+      {/* post adding a product in product from dummyjson.com */}
+      <br />
+      <AddProducts
+        handleAddProduct={handleAddProduct}
+        addedProductData={addedProductData}
+      />
+      <br />
+      <UpdateProducts
+        handleUpdateProduct={handleUpdateProduct}
+        updatedProductData={updatedProductData}
+      />
+    </>
+  );
+};
+export default Users;
